f468826d965308bfea20a1ba86c4369ee2521f8d
artur.zolkos <artur.zolkos@gmail.com>
2020-06-04 16:23:25 +0200

    skeleton

--- rpgtools.py
+++ rpgtools.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def start():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route('/npcgenerator')
+def npcgenerator():
+    return render_template('npcgenerator.html')
 
 if __name__ == '__main__':
     app.run( debug = False)
